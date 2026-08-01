f7919efed8157e66d55b861013f663c3a87afc20
neemiastorres <neemiastorres@gmail.com>
2026-08-01 12:24:26 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,24 @@
+const CACHE_NAME = 'artigoum-v1';
 
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo-a1.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
